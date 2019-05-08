f5740779b0dc8c6780829859955eb362e33ea9c5
Muhdo <gabrielmcv1@gmail.com>
2019-05-08 09:36:55 +0100

    Saving

--- backend/sendNews.php
+++ backend/sendNews.php
@@ -2,8 +2,10 @@
    include_once "database.php";
 
    $titulo = $_POST["titulo"];
+   $titulo = $_POST["titulo"];
    $imagem = $_POST["imagem"];
    $editor = $_POST["editor"];
+   $editor = $_POST["editor"];
 
-   
+
 ?>
--- noticias.php
+++ noticias.php
@@ -8,6 +8,11 @@
       <link rel="stylesheet" href="css/style.css">
       <link rel="stylesheet" href="css/noticias.css">
 
+      <script src="node_modules/jquery/dist/jquery.js"></script>
+      <script src="node_modules/cropperjs/dist/cropper.js"></script>
+      <link href="node_modules/cropperjs/dist/cropper.css" rel="stylesheet">
+      <script src="node_modules/jquery-cropper/dist/jquery-cropper.js"></script>
+
       <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=75m26byuv004g3ef1g0nt44veoaej2ja385dzy5fynrjt9jm"></script>
    </head>
    <body>
@@ -42,17 +47,17 @@
                }
 
                if ($contagem >= 1) {
-                  $queryLoadNoticias = $connection->prepare("SELECT Key_Noticia, Titulo, Imagem, DataPublicacao FROM Noticia ORDER BY DataPublicacao DESC");
+                  $queryLoadNoticias = $connection->prepare("SELECT Key_Noticia, TituloPT, Imagem, DataPublicacao FROM Noticia ORDER BY DataPublicacao DESC");
                   $queryLoadNoticias->execute();
 
                   if ($queryLoadNoticias->rowCount() >= 1) {
                      foreach ($queryLoadNoticias->fetchAll() as $resultado) {
                         $ultimoPost++;
 
-                        if (strlen(utf8_encode($resultado["Titulo"])) > 60) {
-                           $titulo = mb_substr(utf8_encode($resultado["Titulo"]), 0, 60)."<code>...</code>";
+                        if (strlen(utf8_encode($resultado["TituloPT"])) > 60) {
+                           $titulo = mb_substr(utf8_encode($resultado["TituloPT"]), 0, 60)."<code>...</code>";
                         } else {
-                           $titulo = utf8_encode($resultado["Titulo"]);
+                           $titulo = utf8_encode($resultado["TituloPT"]);
                         }
 
                         setlocale(LC_ALL, 'pt_PT', 'pt_PT.utf-8', 'pt_PT.utf-8', 'portuguese');
@@ -75,27 +80,111 @@
             <button type="button" name="NovaNoticia">Nova Notícia</button>
          </div>
          <form action="backend/sendNews.php" method="post">
-            <p>Titulo:<br>
-               <input type="text" name="titulo">
+            <p>Titulo Português:<br>
+               <input type="text" name="tituloPT">
+            </p>
+            <p>Titulo Inglês:<br>
+               <input type="text" name="tituloEN">
             </p>
             <p>Imagem Cabeçalho:<br>
                <label class="form-filebutton">Carregar Imagem
                   <input type="file" id="imagem" name="imagem" accept="image/png, image/jpeg, image/JPEG, image/jpeg2000, image/jpg">
                </label>
             </p>
-            <p>Notícia:<br>
-               <textarea id="editor" name="editor"></textarea>
+            <p>Notícia Português:<br>
+               <textarea id="editor1" name="editorPT"></textarea>
+            </p>
+            <p>Notícia Inglês:<br>
+               <textarea id="editor2" name="editorEN"></textarea>
             </p>
          </form>
+         <div class="div-preview hidden">
+            <img class="img-preview" id="img-preview">
+            <div class="div-buttons">
+               <div class="div-buttons-group">
+                  <button class="btn-selected" type="button" id="btn-move"><img class="img-button" src="img/arrows.png"></button>
+                  <button type="button" id="btn-crop"><img class="img-button" src="img/crop.png"></button>
+               </div>
+               <div class="div-buttons-group">
+                  <button type="button" id="btn-rotLft"><img class="img-button" src="img/rotate-left.png"></button>
+                  <button type="button" id="btn-rotRht"><img class="img-button" src="img/rotate-right.png"></button>
+               </div>
+               <div class="div-buttons-group">
+                  <button type="button" id="btn-zoomIn"><img class="img-button" src="img/zoom-in.png"></button>
+                  <button type="button" id="btn-zoomOut"><img class="img-button" src="img/zoom-out.png"></button>
+               </div>
+               <div class="div-buttons-group">
+                  <button type="button" id="btn-reset"><img class="img-button" src="img/reset.png"></button>
+               </div>
+            </div>
+            <div class="div-buttons">
+               <button class="btn-send" type="button" id="btn-submit">Enviar</button>
+            </div>
+         </div>
       </main>
       <?php } ?>
 
       <script>
+      $(function() {
+         var image = $("#img-preview");
+         $("input:file").change(function() {
+            $(".div-preview").removeClass("hidden");
+
+            var oFReader = new FileReader();
+
+            oFReader.readAsDataURL(this.files[0]);
+            oFReader.onload = function (oFREvent) {
+               image.cropper("destroy");
+               image.attr("src", this.result);
+               image.cropper({
+                  aspectRatio: 1 / 1,
+                  viewMode: 1,
+                  toggleDragModeOnDblclick: false,
+                  dragMode: "move",
+                  crop: function(e) {}
+               });
+            };
+         });
+
+         $("#btn-move").click(function() {
+            $("#btn-crop").removeClass("btn-selected");
+            $("#btn-move").addClass("btn-selected");
+            $("#img-preview").cropper("setDragMode", "move");
+         })
+
+         $("#btn-crop").click(function() {
+            $("#btn-move").removeClass("btn-selected");
+            $("#btn-crop").addClass("btn-selected");
+            $("#img-preview").cropper("setDragMode", "crop");
+         })
+
+         $("#btn-rotLft").click(function() {
+            $("#img-preview").cropper("rotate", -5);
+         })
+
+         $("#btn-rotRht").click(function() {
+            $("#img-preview").cropper("rotate", 5);
+         })
+
+         $("#btn-zoomIn").click(function() {
+            $("#img-preview").cropper("zoom", 0.1);
+         })
+
+         $("#btn-zoomOut").click(function() {
+            $("#img-preview").cropper("zoom", -0.1);
+         })
+
+         $("#btn-reset").click(function() {
+            $("#img-preview").cropper("reset");
+         })
+      });
+
          tinymce.init({
-            selector: '#editor',
-            plugins: "link linkchecker searchreplace media image imagetools visualblocks preview fullscreen tinymcespellchecker emoticons table lists advlist help autosave save",
-            toolbar: "cut copy paste | undo redo | link image | styleselect forecolor backcolor | bold italic underline strikethrough subscript superscript | alignleft aligncenter alignright alignjustify | table bullist numlist outdent indent | help restoredraft save",
-            spellchecker_language: "pt_PT"
+            selector: '#editor1, #editor2',
+            plugins: "link linkchecker searchreplace visualblocks preview fullscreen tinymcespellchecker emoticons table lists advlist help autosave save wordcount",
+            toolbar: "cut copy paste | undo redo | styleselect forecolor backcolor | bold italic underline strikethrough subscript superscript link | alignleft aligncenter alignright alignjustify | table bullist numlist outdent indent | help restoredraft save",
+            spellchecker_language: "pt_PT",
+            default_link_target: "_blank"
          });
       </script>
    </body>
